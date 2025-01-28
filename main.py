0ffb1964c0b82e8785faf0571aff91ae25a042b9
Ruben Ramirez <172532497+Ruben-F-Ramirez@users.noreply.github.com>
2025-01-28 12:55:29 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+
+import time
+
+print("this is in buffer", end=", ")
+
+time.sleep(5)
+
+print("after the flush")
+
+# text_file = open("file.txt", "w")
+
+# print("test 2", file=text_file)
+
+# text_file.close()
